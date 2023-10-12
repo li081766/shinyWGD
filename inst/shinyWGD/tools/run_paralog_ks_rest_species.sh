0ff6a634dcea13c28a19ff40c9dcf9eb7d6c2aa5
li081766 <li081766@gmail.com>
2023-10-12 11:58:07 +0200

    allow to run paralog ks analysis if the species without gff annotation

--- inst/shinyWGD/tools/run_paralog_ks_rest_species.sh
+++ inst/shinyWGD/tools/run_paralog_ks_rest_species.sh
@@ -6,14 +6,14 @@
 #SBATCH -o run_paralog_ks_rest_species.sh.o%j
 #SBATCH -e run_paralog_ks_rest_species.sh.e%j
 
-export OPENBLAS_NUM_THREADS=2
-export GOTO_NUM_THREADS=2
-export OMP_NUM_THREADS=2
+export OPENBLAS_NUM_THREADS=4
+export GOTO_NUM_THREADS=4
+export OMP_NUM_THREADS=4
 
 module load ksrates/x86_64/1.1.3
 
 
-species_names=$(awk -F'[:, ]+' '/fasta_filenames/ {for (i=3; i<=NF; i+=2) print $i}' ksrates_conf.txt)
+species_names=$(ls ../*fa | cut -d '/' -f 2 | sed 's/\.fa//')
 focal_species=$(awk -F'=' '/focal_species/ {gsub(/^[ \t]+|[ \t]+$/, "", $2); print $2}' ksrates_conf.txt)
 
 readarray -t species_array <<< "$species_names"
@@ -21,19 +21,26 @@ readarray -t species_array <<< "$species_names"
 species_array=("${species_array[@]/$focal_species}")
 
 for species in "${species_array[@]}"; do
-	echo $species
-	gff_file="../$species.gff"
-	if [[ -f "$gff_file" ]]; then
-		focal_species="$species"
-		echo "Found $gff_file"
-		awk -v focal_species="$focal_species" -v gff_file="$gff_file" '/focal_species/ {$0 = "focal_species = " focal_species} /gff_filename/ {$0 = "gff_filename = " gff_file} 1' ksrates_conf.txt > ksrates_conf_$species.txt
-		ksrates init ksrates_conf_$species.txt
-		ksrates paralogs-ks ksrates_conf_$species.txt --n-threads 4
-		rm ksrates_conf_$species.txt
-		if [ -f "rate_adjustment/$species.txt" ]; then
-			rm "rate_adjustment/$species.txt" 
-		fi
+	if [[ "$species" == "" ]]; then
+		continue
 	fi
+	
+	gff_file="../$species.gff"
+
+	if [[ -f "$gff_file" ]]; then
+		focal_species_renew="$species"
+		awk -v focal_species="$focal_species_renew" -v gff_file="$gff_file" '/focal_species/ {$0 = "focal_species=" focal_species} /gff_filename/ {$0 = "gff_filename=" gff_file} 1' ksrates_conf.txt > ksrates_conf_$species.txt
+	else
+		focal_species_renew="$species"
+		awk -v focal_species="$focal_species_renew" '/focal_species/ {$0 = "focal_species=" focal_species} 1' ksrates_conf.txt > ksrates_conf_$species.txt
+		sed -i '/gff_filename/d' ksrates_conf_$species.txt
+		sed -i 's/collinearity=yes/collinearity=no/' ksrates_conf_$species.txt
+	fi
+
+	ksrates init ksrates_conf_$species.txt
+	ksrates paralogs-ks ksrates_conf_$species.txt --n-threads 4
+	rm ksrates_conf_$species.txt
+	rm wgd_runs_$species.txt
 done
 
 
